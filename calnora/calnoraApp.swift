4f1762df2181e9026c7ef04e540073c3e4fb0d11
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-05-25 07:55:28 -0400

    Initial Commit

---
+++ calnora/calnoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  calnoraApp.swift
+//  calnora
+//
+//  Created by Gerard Gomez on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct calnoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
